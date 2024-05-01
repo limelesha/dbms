0eb2c703c87be31fd24277c5cb15eb7a5db96027
Aliaksei Kanstantsinau <konstantinov.a.y2005@gmail.com>
2024-05-01 12:11:08 +0200

    Add DB connection config & Product model

---
+++ src/main/java/com/group7/dbms/models/Product.java
@@ -0,0 +1,62 @@
+package com.group7.dbms;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Basic(optional=false)
+    private String name;
+
+    @Basic(optional=false)
+    private String description;
+
+    @Basic(optional=false)
+    private BigDecimal price;
+
+    public Product() {}
+
+    public Product(String name, String description, BigDecimal price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Strng getDescription() { return description; }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public BigDecimal getPrice() { return price; }
+    public void setPrice(BigDecimal price) { this.price = price; }
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Product that = (Product) obj;
+        return Objects.equals(this.id, that.id);
+    }
+
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
