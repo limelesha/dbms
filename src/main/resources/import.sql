65ffb8751d33cd36d68a2b401676bebf42681204
Aliaksei Kanstantsinau <konstantinov.a.y2005@gmail.com>
2024-05-18 16:51:02 +0200

    Setup automatic id assignment from sequence

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,7 @@
-insert into product(id, name, description, price) values (nextval('Product_SEQ'), 'Cupcake', 'Nutritious and imported', 6.33);
-insert into product(id, name, description, price) values (nextval('Product_SEQ'), 'Dickery', 'Do not attempt this at home!', 420.31415);
-insert into product(id, name, description, price) values (nextval('Product_SEQ'), 'Macaron', 'Flavourful filling per customer`s choice', 20);
-insert into product(id, name, description, price) values (nextval('Product_SEQ'), 'Doughnut', 'Tasty douhgnut', 4.99);
+create function assign_id() returns trigger as $$ begin new.id := nextval('Product_SEQ'); return new; end; $$ language plpgsql;
+create trigger assign_id_trigger before insert on product for each row execute function assign_id();
+
+insert into product(name, description, price) values ('Cupcake', 'Nutritious and imported', 6.33);
+insert into product(name, description, price) values ('Dickery', 'Do not attempt this at home!', 420.31415);
+insert into product(name, description, price) values ('Macaron', 'Flavourful filling per customer`s choice', 20);
+insert into product(name, description, price) values ('Doughnut', 'Tasty douhgnut', 4.99);
