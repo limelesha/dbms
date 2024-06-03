652fabaf871bdc1ee5232372fba2f3e4eddf7831
lisikava <mlisikava@gmail.com>
2024-06-03 23:11:45 +0200

    Rewrote controllers

--- src/main/java/com/group7/dbms/controllers/ProductController.java
+++ src/main/java/com/group7/dbms/controllers/ProductController.java
@@ -1,58 +1,95 @@
 package com.group7.dbms;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+import com.eclipsesource.json.JsonArray;
+
+import java.math.BigDecimal;
 
 import spark.Request;
 import spark.Response;
+import spark.Spark;
 
 interface Deserializer<T> {
     public T fromJson(String json, Class<T> objectClass);
 }
 
 public class ProductController {
-
     private ProductsDAO productsDAO;
-    private Deserializer<Product> deserializer;
+    private ProductView productView;
+    //private Deserializer<Product> deserializer;
+    // private static Gson partialRepresentationGson;
+    // private static Gson fullRepresentationGson;
 
-    public ProductController(
-        ProductsDAO productsDAO,
-        Deserializer deserializer
-    ) {
+    public ProductController(ProductsDAO productsDAO) {
         this.productsDAO = productsDAO;
-        this.deserializer = deserializer;
+        productView = new ProductView();
+        //this.deserializer = deserializer;
+        // partialRepresentationGson = Main.setUpGson(RepresentationType.PARTIAL);
+        // fullRepresentationGson = Main.setUpGson(RepresentationType.FULL);
     }
 
-    public Object getByID(Request req, Response res) {
-        Long id = Long.parseLong(req.params(":product-id"));
-        res.status(200);
-        return productsDAO.getByID(id);
-    }
+    public void ignite() {
 
-    public Object getAllProducts(Request req, Response res) {
-        res.status(200);
-        return productsDAO.getAllProducts();
-    }
+        // get by id
+        Spark.get("/products/:product-id", (req, res) -> {
+            Long id = Long.parseLong(req.params(":product-id"));
+            Product product = productsDAO.getByID(id);
+            if (product != null) {
+                res.type("application/json");
+                return productView.dump(product);
+            } else {
+                res.status(404);
+                return "Product not found";
+            }
+        });
 
-    public Object save(Request req, Response res) {
-        Product product = deserializer.fromJson(req.body(), Product.class);
-        product = productsDAO.save(product);
-        res.status(201);
+        
+        // get all
+        Spark.get("/products/", (req, res) -> {
+            res.type("application/json");
+            return productView.dump(productsDAO.getAllProducts());
+        });
+
+        Spark.redirect.get("/products", "/products/");
+
+        // add product
+        Spark.post("/products/", (req, res) -> {
+            //Product product = deserializer.fromJson(req.body(), Product.class);
+            JsonObject json = Json.parse(req.body()).asObject();
+            Product product = deserialize(json);
+            product = productsDAO.save(product);
+            res.status(201);
+            return "Product created";
+        });
+
+        // update product
+        Spark.put("/products/:product-id", (req, res) -> {
+            Long id = Long.parseLong(req.params(":product-id"));
+            JsonObject json = Json.parse(req.body()).asObject();
+            Product product = new Product();
+            product = deserialize(json);
+            product.setId(id);
+            productsDAO.update(product);
+            return "Product updated";
+        });
+
+        // delete product
+        Spark.delete("/products/:product-id", (req, res) -> {
+            Long id = Long.parseLong(req.params(":product-id"));
+            productsDAO.remove(id);
+            return "Product deleted";
+        });
+     }
+
+     private Product deserialize(JsonObject json) {
+        Product product = new Product();
+        product.setName(json.getString("name", ""));
+        product.setPrice(new BigDecimal(json.getString("price", "0")));
+        product.setDescription(json.getString("description", ""));
         return product;
-    }
-
-    public Object update(Request req, Response res) {
-        Long id = Long.parseLong(req.params(":product-id"));
-        Product product = deserializer.fromJson(req.body(), Product.class);
-        product.setId(id);
-        productsDAO.update(product);
-        res.status(200);
-        return "";
-    }
-
-    public Object remove(Request req, Response res) {
-        Long id = Long.parseLong(req.params(":product-id"));
-        productsDAO.remove(id);
-        res.status(200);
-        return "";
-    }
+     }
 }
